db40e2ddd56aa43abf53076669a939618ac67297
Maksim <kuzminovm78@gmail.com>
2025-11-13 19:47:30 +0700

    add page for one project

--- frontend/src/app/providers/RouterProvider.tsx
+++ frontend/src/app/providers/RouterProvider.tsx
@@ -1,16 +1,26 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { LoginPage } from '@/pages/login/LoginPage';
-import { ProjectsPage } from '@/pages/project/ProjectPage';
+import { ProjectsPage } from '@/pages/projectsPage/ProjectsPage';
 import { AuthSuccess } from '@/features/auth-by-google';
 import { ProtectedRoute } from '@/shared/route/ProtectedRoute';
 import { NotFoundPage } from '@/pages/not-found-page/NotFoundPage';
+import { ProjectPage } from '@/pages/projectPage/ProjectPage';
+import { AuthRoute } from '@/shared/route';
 
 const router = createBrowserRouter([
-  { path: '/auth', element: <LoginPage /> },
-  { path: '/auth/success', element: <AuthSuccess /> },
+  {
+    element: <AuthRoute />,
+    children: [
+      { path: '/auth', element: <LoginPage /> },
+      { path: '/auth/success', element: <AuthSuccess /> },
+    ]
+  },
   {
     element: <ProtectedRoute />,
-    children: [{ path: '/', element: <ProjectsPage /> }],
+    children: [
+      { path: '/', element: <ProjectsPage /> },
+      { path: '/project/:id', element: <ProjectPage />}
+    ],
   },
   {
     path: '*', element: <NotFoundPage />
