553cd3fac7ac0783960cf08260581c08e6dd4315
Maksim <kuzminovm78@gmail.com>
2025-11-27 13:52:18 +0700

    castomize login page

--- frontend/src/pages/login/LoginPage.tsx
+++ frontend/src/pages/login/LoginPage.tsx
@@ -1,12 +1,16 @@
+import { ButtonSocial } from '@/shared/ui/button/';
+
 export const LoginPage = () => {
   const handleAuthClick = () => {
     window.location.href = 'http://localhost:8000/auth';
   };
 
   return (
-    <div>
-      <h1>Авторизация</h1>
-      <button onClick={handleAuthClick}>Войти через Google</button>
+    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
+      <h1 className="text-3xl font-semibold">Авторизация</h1>
+      <ButtonSocial onClick={handleAuthClick} variant="destructive">
+        Войти
+      </ButtonSocial>
     </div>
   );
 };
