9ff4ed8a12cdeb7f3efab229da8410c8e6b7929b
Dimandr <Dmitry221556@yandex.ru>
2025-12-18 23:01:00 +0700

    test updated with webtestclient

--- backend/src/test/java/application/api/projects/ProjectControllerTest.java
+++ backend/src/test/java/application/api/projects/ProjectControllerTest.java
@@ -1,40 +1,35 @@
 package application.api.projects;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import application.testconfig.TestContainersInitializer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@ContextConfiguration(initializers = TestContainersInitializer.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProjectControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
 
+    @Autowired
+    private WebTestClient webClient;
     @Test
     void apiGetProjects_BadUser() throws Exception {
-        mockMvc.perform(get("/projects/123"))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.status").value(400))
-                .andExpect(jsonPath("$.error").value("Wrong request parameter"))
-                .andExpect(jsonPath("$.timestamp").exists())
-                .andExpect(jsonPath("$.path").value("/projects/123"));;
+        webClient.get().uri("/projects/123")
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody()
+                .jsonPath("$.status").isEqualTo("400")
+                .jsonPath("$.error").isEqualTo("Wrong request parameter")
+                .jsonPath("$.timestamp").exists()
+                .jsonPath("$.path").isEqualTo("/projects/123");
     }
 
     @Test
-    void apiGetProjects_NoSuchUser() throws Exception {
-        mockMvc.perform(get("/projects/267b4f92-09d5-4273-8209-ad337c0"))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.status").value(400))
-                .andExpect(jsonPath("$.error").value("User not found"))
-                .andExpect(jsonPath("$.timestamp").exists())
-                .andExpect(jsonPath("$.path").value("/projects/267b4f92-09d5-4273-8209-ad337c0"));;
+    void apiGetProjects_NoSuchUser(@Autowired WebTestClient webClient) throws Exception {
+        webClient.get().uri("/projects/267b4f92-09d5-4273-8209-ad337c0")
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody()
+                .jsonPath("$.status").isEqualTo("400")
+                .jsonPath("$.error").isEqualTo("User not found")
+                .jsonPath("$.timestamp").exists()
+                .jsonPath("$.path").isEqualTo("/projects/267b4f92-09d5-4273-8209-ad337c0");
     }
 }
