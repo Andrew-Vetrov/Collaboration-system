3b12c5dae719a5589f21126d11d799f8eb5329fe
Andrew_Vetrov <user120715mts@gmail.com>
2026-03-20 15:28:22 +0300

    remove camelCase in InviteResponseDto

--- backend/src/main/java/application/dtos/responses/InviteResponseDto.java
+++ backend/src/main/java/application/dtos/responses/InviteResponseDto.java
@@ -1,5 +1,6 @@
 package application.dtos.responses;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,12 +12,27 @@ import java.util.UUID;
 @Setter
 @Builder
 public class InviteResponseDto {
+    @JsonProperty("invite_id")
     private UUID inviteId;
+
+    @JsonProperty("project_id")
     private UUID projectId;
+
+    @JsonProperty("email")
     private String email;
+
+    @JsonProperty("invited_at")
     private LocalDateTime invitedAt;
+
+    @JsonProperty("sender_nickname")
     private String senderNickname;
+
+    @JsonProperty("project_name")
     private String projectName;
+
+    @JsonProperty("receiver_nickname")
     private String receiverNickname;
+
+    @JsonProperty("receiver_avatar")
     private String receiverAvatar;
 }
