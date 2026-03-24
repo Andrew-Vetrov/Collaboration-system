5732db89a101b200676c2b3a1507860e4745ade4
Maksim <kuzminovm78@gmail.com>
2026-03-24 14:38:15 +0700

    fix time and date display

--- frontend/src/shared/lib/utils.ts
+++ frontend/src/shared/lib/utils.ts
@@ -1,6 +1,25 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function parseDateTime(time: string) {
+  if (time == '') {
+    return;
+  }
+  const splitted = time.split(new RegExp('T|Z'));
+  if (splitted.length !== 3) {
+    return;
+  }
+  const splittedDate = splitted[0].split('-');
+  return {
+    time: splitted[1],
+    date: {
+      year: splittedDate[0],
+      month: splittedDate[1],
+      day: splittedDate[2],
+    },
+  };
 }
