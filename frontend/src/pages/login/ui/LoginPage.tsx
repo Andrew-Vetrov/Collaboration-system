c3d6f90d2781ee5c3e751ede3316cf372c24706d
Maksim <kuzminovm78@gmail.com>
2026-03-26 20:16:46 +0700

    privacy policy and user agent on login page

--- frontend/src/pages/login/ui/LoginPage.tsx
+++ frontend/src/pages/login/ui/LoginPage.tsx
@@ -6,12 +6,24 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
+  Dialog,
+  DialogContent,
+  Input,
 } from '@/shared/ui/';
+import { Checkbox } from '@/shared/ui/checkbox';
+import { useState } from 'react';
+import { PrivacyPolicy } from './PrivacyPolicy';
+import { UserAgent } from './UserAgent';
 
 export const LoginPage = () => {
   const handleAuthClick = () => {
     window.location.href = 'http://localhost:8080/login-start';
   };
+
+  const [flag, setFlag] = useState<boolean>(false);
+
+  const [isUserAgentOpen, setIsUserAgentOpen] = useState<boolean>(false);
+  const [isPrivacyPolicy, setPrivacyPolicy] = useState<boolean>(false);
 
   return (
     <>
@@ -20,7 +32,7 @@ export const LoginPage = () => {
           <ModeToggle />
         </div>
       </header>
-      <div className="flex items-center justify-center min-h-screen">
+      <div className="flex items-center flex-col gap-2 justify-center min-h-screen">
         <Card className="w-full max-w-sm p-4">
           <CardHeader>
             <CardTitle className="text-center text-3xl font-semibold">
@@ -36,10 +48,49 @@ export const LoginPage = () => {
               onClick={handleAuthClick}
               variant="outline"
               className="rounded-md"
+              disabled={!flag}
             />
           </CardContent>
         </Card>
+        <div className="w-full max-w-sm flex gap-4">
+          <Checkbox
+            checked={flag}
+            onCheckedChange={() => setFlag(prev => !prev)}
+            className="rounded-md w-9 h-9 border-primary"
+          />
+          <span>
+            Нажимая «Авторизация через Google», вы принимаете условия{' '}
+            <b
+              className="cursor-pointer"
+              onClick={() => {
+                setIsUserAgentOpen(true);
+              }}
+            >
+              Пользовательского соглашения
+            </b>{' '}
+            и даете согласие на обработку своих персональных данных в
+            соответствии с{' '}
+            <b
+              className="cursor-pointer"
+              onClick={() => {
+                setPrivacyPolicy(true);
+              }}
+            >
+              Политикой конфиденциальности
+            </b>
+          </span>
+        </div>
       </div>
+      <Dialog open={isUserAgentOpen} onOpenChange={setIsUserAgentOpen}>
+        <DialogContent className="max-h-[70vh] overflow-y-scroll">
+          <UserAgent />
+        </DialogContent>
+      </Dialog>
+      <Dialog open={isPrivacyPolicy} onOpenChange={setPrivacyPolicy}>
+        <DialogContent className="max-h-[70vh] overflow-y-scroll">
+          <PrivacyPolicy />
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
