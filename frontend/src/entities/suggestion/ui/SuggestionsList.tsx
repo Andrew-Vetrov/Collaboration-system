3ea150955dab75ddbc4f496400ea83aac95757ba
Andrew-Vetrov <145760988+Andrew-Vetrov@users.noreply.github.com>
2026-05-21 10:07:11 +0300

    Refactor SuggestionsList for better user handling

--- frontend/src/entities/suggestion/ui/SuggestionsList.tsx
+++ frontend/src/entities/suggestion/ui/SuggestionsList.tsx
@@ -18,7 +18,9 @@ interface SuggestionsListProps {
   projectId: string;
 }
 
-export const SuggestionsList = (props: SuggestionsListProps): JSX.Element => {
+export const SuggestionsList = (
+  props: SuggestionsListProps
+): JSX.Element => {
   const {
     data: suggestions = [],
     isLoading: isSuggestionsLoading,
@@ -31,91 +33,116 @@ export const SuggestionsList = (props: SuggestionsListProps): JSX.Element => {
     isError: isProjectUsersError,
   } = useProjectUsers(props.projectId);
 
-  const suggestionsWithUsers = suggestions.map(suggestion => {
-    const user = projectUsers?.users.filter(
-      user => user.user_id === suggestion.user_id
-    )[0]!;
-    const result = { ...suggestion, user };
-    return result;
-  });
+  const suggestionsWithUsers = suggestions
+    .map(suggestion => {
+      const user = projectUsers?.users.find(
+        user => user.user_id === suggestion.user_id
+      );
+
+      if (!user) return null;
+
+      return {
+        ...suggestion,
+        user,
+      };
+    })
+    .filter(Boolean);
 
   if (isSuggestionsLoading || isProjectUsersLoading) {
     return <div>Загрузка предложений...</div>;
   }
 
   if (isSuggestionsError || isProjectUsersError) {
-    return <div>Ошибка загрузки предложений</div>;
+    return <div>Ошибка  загрузки предложений</div>;
   }
+
   return (
     <div className="flex flex-col items-center w-full">
       <h1 className="text-center text-3xl my-4">Предложения</h1>
+
       <div className="grid gap-4 w-[90%] sm:w-[70%] grid-cols-1">
         {suggestionsWithUsers
-          .filter(suggestion => suggestion.status != 'draft')
-          .map(suggestion => (
-            <Card
-              key={suggestion.suggestion_id}
-              className="p-6 hover:shadow-lg transition-shadow duration-200"
-            >
-              <Link
-                className="flex flex-col sm:flex-row sm:justify-between gap-4"
-                to={routes.suggestionRoute(
-                  props.projectId,
-                  suggestion.suggestion_id || ''
-                )}
+          .filter(suggestion => suggestion?.status !== 'draft')
+          .map(suggestion => {
+            if (!suggestion) return null;
+
+            return (
+              <Card
+                key={suggestion.suggestion_id}
+                className="p-6 hover:shadow-lg transition-shadow duration-200"
               >
-                <div className="flex flex-col flex-1 items-start justify-between min-w-0 gap-6 mb-4">
-                  <div>
-                    <CardTitle className="text-lg font-semibold line-clamp-2 mb-2">
-                      {suggestion.name}
-                    </CardTitle>
-                    <CardDescription className="line-clamp-3 leading-relaxed whitespace-pre-line">
-                      {suggestion.description}
-                    </CardDescription>
-                  </div>
-                  <div className="flex gap-2 items-center min-w-0">
-                    <Avatar className="h-9 w-9 shrink-0">
-                      <AvatarImage
-                        src={suggestion.user.avatar_url}
-                        alt="Аватар пользователя"
-                      />
-                    </Avatar>
-                    <div className="min-w-0 max-w-full break-all text-left">
-                      {suggestion.user.nickname}
+                <Link
+                  className="flex flex-col sm:flex-row sm:justify-between gap-4"
+                  to={routes.suggestionRoute(
+                    props.projectId,
+                    suggestion.suggestion_id || ''
+                  )}
+                >
+                  <div className="flex flex-col flex-1 items-start justify-between min-w-0 gap-6 mb-4">
+                    <div>
+                      <CardTitle className="text-lg font-semibold line-clamp-2 mb-2">
+                        {suggestion.name}
+                      </CardTitle>
+
+                      <CardDescription className="line-clamp-3 leading-relaxed whitespace-pre-line">
+                        {suggestion.description}
+                      </CardDescription>
+                    </div>
+
+                    <div className="flex gap-2 items-center min-w-0">
+                      <Avatar className="h-9 w-9 shrink-0">
+                        <AvatarImage
+                          src={suggestion.user?.avatar_url}
+                          alt="Аватар пользователя"
+                        />
+                      </Avatar>
+
+                      <div className="min-w-0 max-w-full break-all text-left">
+                        {suggestion.user?.nickname ||
+                          'Неизвестный пользователь'}
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="flex flex-col shrink-0 gap-4 items-end">
-                  <div className="px-3 py-1.5 rounded-full text-xs font-medium bg-gray-100 text-gray-700">
-                    {STATUS_LABELS[suggestion.status] || suggestion.status}
-                  </div>{' '}
-                  <div className="flex gap-3">
-                    <div className="flex gap-1 text-sm">
-                      <Heart className="fill-(--color-filled-heart)" /> {suggestion.user_likes_amount}
+
+                  <div className="flex flex-col shrink-0 gap-4 items-end">
+                    <div className="px-3 py-1.5 rounded-full text-xs font-medium bg-gray-100 text-gray-700">
+                      {STATUS_LABELS[suggestion.status] ||
+                        suggestion.status}
                     </div>
-                    <div className="flex gap-1 text-sm">
-                      <Heart className="" /> {suggestion.likes_amount}
+
+                    <div className="flex gap-3">
+                      <div className="flex gap-1 text-sm">
+                        <Heart className="fill-(--color-filled-heart)" />
+                        {suggestion.user_likes_amount}
+                      </div>
+
+                      <div className="flex gap-1 text-sm">
+                        <Heart />
+                        {suggestion.likes_amount}
+                      </div>
                     </div>
-                  </div>
-                  <div>
-                    {(() => {
-                      const parsedTime = parseDateTime(
-                        suggestion.placed_at || ''
-                      );
-                      return (
-                        <>
+
+                    <div>
+                      {(() => {
+                        const parsedTime = parseDateTime(
+                          suggestion.placed_at || ''
+                        );
+
+                        return (
                           <span>
-                            {parsedTime?.time} {parsedTime?.date.day}-
-                            {parsedTime?.date.month}-{parsedTime?.date.year}
+                            {parsedTime?.time}{' '}
+                            {parsedTime?.date.day}-
+                            {parsedTime?.date.month}-
+                            {parsedTime?.date.year}
                           </span>
-                        </>
-                      );
-                    })()}
+                        );
+                      })()}
+                    </div>
                   </div>
-                </div>
-              </Link>
-            </Card>
-          ))}
+                </Link>
+              </Card>
+            );
+          })}
       </div>
     </div>
   );
